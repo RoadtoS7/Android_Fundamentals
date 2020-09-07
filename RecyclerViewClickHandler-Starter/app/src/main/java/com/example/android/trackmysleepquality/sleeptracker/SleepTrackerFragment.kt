4281ad0ffbeeafbd22b3d6a25c7382ee7a6b2675
RoadtoS7 <kshvv975@gmail.com>
2020-09-07 20:49:00 +0900

    Update spanSizeLookup configuration GridLayoutManager는 spanSizeLookup이라는 설정을 가지고 있음 이 설정을 통해서 아이템이 차지하는 span을 조정할 수 있다. spanSizeLookup은 객체를 받기 때문에, 익명객체를 생성하여 넣어준다.

--- RecyclerViewClickHandler-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ RecyclerViewClickHandler-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -132,6 +132,16 @@ class SleepTrackerFragment : Fragment() {
 
         val manager = GridLayoutManager(activity, 3)
         binding.sleepList.layoutManager = manager
+        manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return when (position) {
+                    0 -> 3
+                    else -> 1
+                }
+
+            }
+
+        }
 
 
         return binding.root
