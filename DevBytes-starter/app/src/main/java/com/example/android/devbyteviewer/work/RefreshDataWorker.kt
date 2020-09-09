89b480c1547e293e4e25f5c4d4f163ec92f34bf2
RoadtoS7 <kshvv975@gmail.com>
2020-09-09 18:49:48 +0900

    Add RefreshDataWorker.kt WorkManager에서 Work란, 백그라운드에서 실행될 작업을 의미한다. Worker클래스를 상속받아서, Worker클래스의 doWork()함수를 오버라이딩한다. 백그라운드에서 실행될 작업을 doWork()함수안에다가 정의한다. doWork()함수는 백그라운드 스레드에서 실행된다. 이 메서드는 동기적으로 작동한다. 반드시 ListenableWorker.Result 객체를 반환해야 한다. 안드로이드 시스템은 하나의 Worker객체당 실행시간을 10분만 준다. 10분을 초과하면 시스템은 Worker의 작업을 중단시킨다. Worker의 실행결과를 나타내는 함수를 호출하면, ListenableWorker.Result가 생성된다.

---
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWorker.kt
@@ -0,0 +1,27 @@
+package com.example.android.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.android.devbyteviewer.database.DatabaseVideo
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
+import timber.log.Timber
+import java.lang.Exception
+
+class RefreshDataWorker(context: Context, params: WorkerParameters)
+    : CoroutineWorker(context, params) {
+    override suspend fun doWork(): Result {
+        val dataBase = getDatabase(applicationContext)
+        val repository = VideosRepository(dataBase)
+        try{
+            repository.refreshVideos()
+            Timber.d("Work request for async is run")
+        }catch (e: HttpException){
+            return Result.retry()
+        }
+
+        return Result.success()
+    }
+}
