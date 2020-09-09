2a0c9bae5c1dd86d1b476a5736c9e9922bca3855
RoadtoS7 <kshvv975@gmail.com>
2020-09-09 21:31:14 +0900

    Add multiple constraints to repeatingRequest WorkRequest에 여러개의 제약을 검

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -17,9 +17,8 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
+import android.os.Build
+import androidx.work.*
 import com.example.android.devbyteviewer.work.RefreshDataWorker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -45,20 +44,31 @@ class DevByteApplication : Application() {
 
     }
 
-    private fun setupRecurringWork(){
+    private fun setupRecurringWork() {
+        val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .setRequiresBatteryNotLow(true)
+                .setRequiresCharging(true)
+                .apply {
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                        setRequiresDeviceIdle(true)
+                    }
+                }
+                .build()
 //        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(
 //                1, TimeUnit.DAYS)
 //                .build()
-        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(
-                15, TimeUnit.MINUTES
-        ).build()
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+                .setConstraints(constraints)
+                .build()
+        Timber.d("Periodic work request for sync is scheduled")
         WorkManager.getInstance().enqueueUniquePeriodicWork(
                 RefreshDataWorker.WORK_NAME,
                 ExistingPeriodicWorkPolicy.KEEP,
                 repeatingRequest)
     }
 
-    private fun delayInit(){
+    private fun delayInit() {
         applicationScope.launch {
             Timber.plant(Timber.DebugTree())
             setupRecurringWork()
