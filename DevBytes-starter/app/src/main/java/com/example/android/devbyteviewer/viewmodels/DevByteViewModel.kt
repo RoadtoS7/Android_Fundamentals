761e249508743d240a5705fcf356c53e898f79fc
RoadtoS7 <kshvv975@gmail.com>
2020-09-09 17:16:01 +0900

    Add repository to ViewModel 데이터베이스 리프레쉬 정책으로 아주 간단한 것을 적용함. 여기서 쓰인 레포지터리에 데이터를 요청하는 모듈이 데이터 리프레시 역할을 하는 정책을 사용했다. 데이터베이스 리프레쉬란 로컬에 저장된 데이터를 서버에 저장된 데이터 일치시키는 것을 말한다. ViewModel내의 playlist 변수를 레포지터리 내의 playlist 변수값으로 할당했다. 레포지터리 내의 playlist 변수는 데이터베이스 내의 데이터가 변경될 때, 데이터베이스의 데이터를 들고와서 업데이트 된다. 따라서 ViewModel에서 데이터베이스 리프레쉬 작업을 실시하면, 서버에 있는 데이터가 데이터베이스에 저장되고, 업데이트 된 데이터가 ViewModel의 playlist에 자동으로 반영된다.

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
@@ -23,11 +23,16 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.example.android.devbyteviewer.database.DatabaseVideo
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.getDatabase
 import com.example.android.devbyteviewer.domain.DevByteVideo
 import com.example.android.devbyteviewer.network.DevByteNetwork
 import com.example.android.devbyteviewer.network.asDomainModel
+import com.example.android.devbyteviewer.repository.VideosRepository
 import kotlinx.coroutines.*
 import java.io.IOException
+import java.lang.Exception
 
 /**
  * DevByteViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
@@ -40,21 +45,8 @@ import java.io.IOException
  * or fragment lifecycle events.
  */
 class DevByteViewModel(application: Application) : AndroidViewModel(application) {
-
-    /**
-     * A playlist of videos that can be shown on the screen. This is private to avoid exposing a
-     * way to set this value to observers.
-     */
-    private val _playlist = MutableLiveData<List<DevByteVideo>>()
-
-    /**
-     * A playlist of videos that can be shown on the screen. Views should use this to get access
-     * to the data.
-     */
-    val playlist: LiveData<List<DevByteVideo>>
-        get() = _playlist
-
-
+    private val videosRepository = VideosRepository(getDatabase(application))
+    val playlist = videosRepository.videos
 
     /**
      * Event triggered for network error. This is private to avoid exposing a
@@ -86,26 +78,40 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
      * init{} is called immediately when this ViewModel is created.
      */
     init {
-        refreshDataFromNetwork()
+        refreshDataFromRepository()
     }
 
+    private fun refreshDataFromRepository(){
+        viewModelScope.launch {
+            try{
+                videosRepository.refreshVideos()
+                _eventNetworkError.value= false
+                _isNetworkErrorShown.value = false
+            }catch (e: Exception){
+                if(playlist.value.isNullOrEmpty()){
+                    _eventNetworkError.value = true
+                }
+
+            }
+        }
+    }
     /**
      * Refresh data from network and pass it via LiveData. Use a coroutine launch to get to
      * background thread.
      */
     private fun refreshDataFromNetwork() = viewModelScope.launch {
-
-        try {
-             val playlist = DevByteNetwork.devbytes.getPlaylist()
-            _playlist.postValue(playlist.asDomainModel())
-
-            _eventNetworkError.value = false
-            _isNetworkErrorShown.value = false
-
-        } catch (networkError: IOException) {
-            // Show a Toast error message and hide the progress bar.
-            _eventNetworkError.value = true
-        }
+//
+//        try {
+//             val playlist = DevByteNetwork.devbytes.getPlaylist()
+//            _playlist.postValue(playlist.asDomainModel())
+//
+//            _eventNetworkError.value = false
+//            _isNetworkErrorShown.value = false
+//
+//        } catch (networkError: IOException) {
+//            // Show a Toast error message and hide the progress bar.
+//            _eventNetworkError.value = true
+//        }
     }
 
     /**
