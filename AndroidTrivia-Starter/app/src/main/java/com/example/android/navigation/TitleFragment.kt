d41afe24395f56951a82359a0ff20915caf86003
RoadtoS7 <kshvv975@gmail.com>
2020-09-22 18:36:23 +0900

    Add playButton listener to playButton playbutton 을 누르면 GameFragment로 이동하는 네비게이트를 실행하도록 리스너를 달았다.

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -15,6 +16,10 @@ class TitleFragment : Fragment() {
                 R.layout.fragment_title,
                 container,
                 false)
+
+        binding.playButton.setOnClickListener {
+            this.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return binding.root
     }
 }
