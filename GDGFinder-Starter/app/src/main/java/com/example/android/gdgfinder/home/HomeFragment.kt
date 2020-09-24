56b51ae091f86d15626bf666d075e9f4e0864814
RoadtoS7 <kshvv975@gmail.com>
2020-09-24 18:18:18 +0900

    func: Add navigate function when FAB is clicked FAB 가 클릭되면 ViewHolder에서 클릭되었음을 나타내는 변수가 변화시킨다. 뷰에서는 해당 변수를 관찰하고 있다가 변경된 값을 받은 후, 다음 뷰로 이동시킨다.

--- GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ GDGFinder-Starter/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -22,7 +22,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
@@ -38,8 +40,18 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = HomeFragmentBinding.inflate(inflater)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        binding.viewModel = viewModel
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer {navigate ->
+            if(navigate){
+                this.findNavController()
+                    .navigate(HomeFragmentDirections.actionHomeFragmentToGdgListFragment())
+                viewModel.onNavigatedToSearch()
+            }
+
+        })
         return binding.root
     }
 }
